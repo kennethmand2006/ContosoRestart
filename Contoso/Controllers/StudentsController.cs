ea3aade7c5bfadb99788f23c2f7cc112ca5c05fa
Opilane TTHK <opilane@tthk.ee>
2024-11-18 18:36:01 +0200

    Tegin student modeli

---
+++ Contoso/Controllers/StudentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contoso.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Contoso/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace Contoso.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int EnrollmentDate { get; set; }
+
+        //public ICollection<Enrollment>? Enrollments { get; set; }
+
+    }
+}
